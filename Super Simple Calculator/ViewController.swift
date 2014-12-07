15f211138e73ca918428cffb558b571b158f6556
Todd Resudek <info@supersimple.org>
2014-12-07 12:14:08 -0700

    Initial Commit

---
+++ Super Simple Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Super Simple Calculator
+//
+//  Created by Todd Resudek on 12/7/14.
+//  Copyright (c) 2014 supersimple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
